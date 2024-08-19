50c6f186c2f1768da5c908c02479203df15374a5
ShingloDanglo <143428779+ShingloDanglo@users.noreply.github.com>
2024-08-19 17:49:44 +1000

    Rewrote it so it actually works
    Rewrote most of it so that it actually works how it is supposed to. Generated mazes are now much better looking

--- maze.py
+++ maze.py
@@ -1,8 +1,10 @@
 import random
 import os
 import time
-width = 65
-height = 65
+import sys
+sys.setrecursionlimit(10000)
+width = 70
+height = 70
 count = 0
 matrix=[]
 for row in range(width):
@@ -12,7 +14,8 @@ for row in range(width):
     matrix.append(a)
 
 startingX = random.randint(1,width-2)
-startingY = random.randint(0,0)
+startingY = random.randint(1,1)
+
 
 
 
@@ -39,31 +42,46 @@ def printGap():
     
 def checkUp(x,y,count,lastDirection):
     if y > 1 and is_within_bounds(x, y-1) and is_within_bounds(x, y-2) and is_within_bounds(x-1, y-2) and is_within_bounds(x+1, y-2):
-        #print("up")
+        print("up")
         if(matrix[x][y-1]!=1) and (matrix[x][y-2]==0) and (matrix[x-1][y-1]==0) and (matrix[x+1][y-1]==0) and (matrix[x-1][y-2]==0) and (matrix[x+1][y-2]==0):
-            #print("went up")
-            nextCell(x,y-1, count, lastDirection)
-                    
+            print("went up")
+            return True
+        else:
+            return False
+    else:
+        return False
 def checkDown(x,y,count,lastDirection):
     if y < height - 2 and is_within_bounds(x, y+1) and is_within_bounds(x, y+2) and is_within_bounds(x-1, y+2) and is_within_bounds(x+1, y+2):
-        #print("down")
+        print("down")
         if(matrix[x][y+1]!=1) and (matrix[x][y+2]==0) and (matrix[x-1][y+1]==0) and (matrix[x+1][y+1]==0) and (matrix[x-1][y+2]==0) and (matrix[x+1][y+2]==0):
-            #print(" went down")
-            nextCell(x,y+1, count,lastDirection)
+            print(" went down")
+            return True
+        else:
+            return False
+    else:
+        return False
             
 def checkRight(x,y,count,lastDirection):
     if x < width - 2 and is_within_bounds(x+1, y) and is_within_bounds(x+2, y) and is_within_bounds(x+2, y-1) and is_within_bounds(x+2, y+1):
-        #print("right")
+        print("right")
         if(matrix[x+1][y]!=1) and (matrix[x+2][y]==0) and (matrix[x+1][y-1]==0) and (matrix[x+1][y+1]==0) and (matrix[x+2][y-1]==0) and (matrix[x+2][y+1]==0):
-            #print(" went right")
-            nextCell(x+1,y, count,lastDirection)
+            print(" went right")
+            return True
+        else:
+            return False
+    else:
+        return False
             
 def checkLeft(x,y,count,lastDirection):
     if x > 1 and is_within_bounds(x-1, y) and is_within_bounds(x-2, y) and is_within_bounds(x-2, y-1) and is_within_bounds(x-2, y+1):
-        #print("left")
+        print("left")
         if(matrix[x-1][y]!=1) and (matrix[x-2][y]==0) and (matrix[x-1][y-1]==0) and (matrix[x-1][y+1]==0) and (matrix[x-2][y-1]==0) and (matrix[x-2][y+1]==0):
-            #print(" went left")
-            nextCell(x-1,y, count,lastDirection)
+            print(" went left")
+            return True
+        else:
+            return False
+    else:
+        return False
             
 def randomCell(count,lastDirection):
     while True:
@@ -71,6 +89,7 @@ def randomCell(count,lastDirection):
         newY = random.randint(0,height-1)
         if(matrix[newX][newY]==1):
             nextCell(newX, newY, count, lastDirection)
+            print("Jumped to ",newX,newY)
             break
 
 def selectEndpoint():
@@ -93,48 +112,89 @@ def nextCell(x, y, count, lastDirection):
     
     count += 1
     print(count)
-    if count > (width+height)/2:
+    #if count > (width+height)/2:
+    if count > width*height:
+        #time.sleep(0.1)
         #drawMaze()
         return
     
-    if(random.randint(1,2) == 1):
-        direction = lastDirection
+    if(random.randint(1,10) == 1):
+        direction = random.randint(1,110)
     else:
-        direction = random.randint(1,101)
+        direction = lastDirection
     
+    #Check up first
     if(direction >= 1) and (direction <= 25):
-        checkUp(x,y, count, direction)
-        checkRight(x,y, count, direction)
-        checkDown(x,y, count, direction)
-        checkLeft(x,y, count, direction)
+        if(checkUp(x,y, count, direction)):
+            nextCell(x,y-1, count, 1)
+            return
+        if(checkRight(x,y, count, direction)):
+            nextCell(x+1,y, count,26)
+            return
+        if(checkDown(x,y, count, direction)):
+            nextCell(x,y+1, count,51)
+            return
+        if(checkLeft(x,y, count, direction)):
+            nextCell(x-1,y, count,76)
+            return
+        randomCell(count,direction)
+    #Check right first
+    elif(direction >= 26) and (direction <= 50):
+        if(checkRight(x,y, count, direction)):
+            nextCell(x+1,y, count,26)
+            return
+        if(checkLeft(x,y, count, direction)):
+            nextCell(x-1,y, count,76)
+            return
+        if(checkDown(x,y, count, direction)):
+            nextCell(x,y+1, count,51)
+            return
+        if(checkUp(x,y, count, direction)):
+            nextCell(x,y-1, count, 1)
+            return
+        
         randomCell(count,direction)
 
-                
-    elif(direction >= 26) and (direction <= 50):
-        checkRight(x,y, count, direction)
-        checkDown(x,y, count, direction)
-        checkLeft(x,y, count, direction)
-        checkUp(x,y, count, direction)
-        randomCell(count,direction)
-                
+    #Check down first
     elif(direction >= 51) and (direction <= 75):
-        checkDown(x,y, count, direction)
-        checkLeft(x,y, count, direction)
-        checkUp(x,y, count, direction)
-        checkRight(x,y, count, direction)
+        if(checkDown(x,y, count, direction)):
+            nextCell(x,y+1, count,51)
+            return
+        if(checkRight(x,y, count, direction)):
+            nextCell(x+1,y, count,26)
+            return
+        if(checkLeft(x,y, count, direction)):
+            nextCell(x-1,y, count,76)
+            return
+        if(checkUp(x,y, count, direction)):
+            nextCell(x,y-1, count, 1)
+            return
+        
+        randomCell(count,direction)          
+    
+    #Check left first
+    elif(direction >= 75) and (direction <= 100):
+        if(checkLeft(x,y, count, direction)):
+            nextCell(x-1,y, count,76)
+            return
+        if(checkUp(x,y, count, direction)):
+            nextCell(x,y-1, count, 1)
+            return
+        if(checkRight(x,y, count, direction)):
+            nextCell(x+1,y, count,26)
+            return
+        if(checkDown(x,y, count, direction)):
+            nextCell(x,y+1, count,51)
+            return
+        
+        randomCell(count,direction)  
+        
+    elif(direction > 100):
         randomCell(count,direction)
-                
-    elif(direction >= 76) and (direction <= 100):
-        checkLeft(x,y, count, direction)
-        checkUp(x,y, count, direction)
-        checkRight(x,y, count, direction)
-        checkDown(x,y, count, direction)
-        randomCell(count,direction)
-    else:
-        randomCell(count,direction)
+    
             
  
-
-nextCell(startingX, startingY, count, 1)
-#selectEndpoint()
+matrix[startingX][0]=1
+nextCell(startingX, startingY, count, random.randint(1,25))
+selectEndpoint()
 drawMaze()
